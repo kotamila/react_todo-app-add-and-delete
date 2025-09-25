132f2ff7162bac3d46c795fc49deb78b58848e98
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-09-25 14:26:44 +0300

    try to fix error on close button

--- src/components/ErrorNotification.tsx
+++ src/components/ErrorNotification.tsx
@@ -5,17 +5,23 @@ interface Props {
   onHide: () => void;
 }
 
-export const ErrorNotification: React.FC<Props> = ({ message, onHide }) => (
-  <div
-    data-cy="ErrorNotification"
-    className={`notification is-danger is-light ${!message ? 'hidden' : ''}`}
-  >
-    <button
-      data-cy="HideErrorButton"
-      type="button"
-      className="delete"
-      onClick={onHide}
-    />
-    {message}
-  </div>
-);
+export const ErrorNotification: React.FC<Props> = ({ message, onHide }) => {
+  if (!message) {
+    return null;
+  }
+
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={`notification is-danger is-light`}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={onHide}
+      />
+      {message}
+    </div>
+  );
+};
