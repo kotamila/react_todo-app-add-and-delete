f184b0f896fc4ec1d4ad9413a4a0c6293fb285e2
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-10-10 17:51:38 +0300

    create itemText outside JSX

--- src/components/TodoFooter.tsx
+++ src/components/TodoFooter.tsx
@@ -16,53 +16,59 @@ export const TodoFooter: React.FC<Props> = ({
   setFilter,
   hasCompleted,
   onClearCompleted,
-}) => (
-  <footer className="todoapp__footer" data-cy="Footer">
-    <span className="todo-count" data-cy="TodosCounter">
-      {activeCount} {activeCount === 1 ? 'item' : 'items'} left
-    </span>
+}) => {
+  const itemText = `${activeCount} ${activeCount === 1 ? 'item' : 'items'} left`;
 
-    <ul className="filter" data-cy="Filter">
-      <li>
-        <a
-          href="#/"
-          className={classNames({ selected: filter === FilterType.All })}
-          data-cy="FilterLinkAll"
-          onClick={() => setFilter(FilterType.All)}
-        >
-          All
-        </a>
-      </li>
-      <li>
-        <a
-          href="#/active"
-          className={classNames({ selected: filter === FilterType.Active })}
-          data-cy="FilterLinkActive"
-          onClick={() => setFilter(FilterType.Active)}
-        >
-          Active
-        </a>
-      </li>
-      <li>
-        <a
-          href="#/completed"
-          className={classNames({ selected: filter === FilterType.Completed })}
-          data-cy="FilterLinkCompleted"
-          onClick={() => setFilter(FilterType.Completed)}
-        >
-          Completed
-        </a>
-      </li>
-    </ul>
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {itemText}
+      </span>
 
-    <button
-      type="button"
-      className="todoapp__clear-completed"
-      data-cy="ClearCompletedButton"
-      onClick={onClearCompleted}
-      disabled={!hasCompleted}
-    >
-      Clear completed
-    </button>
-  </footer>
-);
+      <ul className="filter" data-cy="Filter">
+        <li>
+          <a
+            href="#/"
+            className={classNames({ selected: filter === FilterType.All })}
+            data-cy="FilterLinkAll"
+            onClick={() => setFilter(FilterType.All)}
+          >
+            All
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/active"
+            className={classNames({ selected: filter === FilterType.Active })}
+            data-cy="FilterLinkActive"
+            onClick={() => setFilter(FilterType.Active)}
+          >
+            Active
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/completed"
+            className={classNames({
+              selected: filter === FilterType.Completed,
+            })}
+            data-cy="FilterLinkCompleted"
+            onClick={() => setFilter(FilterType.Completed)}
+          >
+            Completed
+          </a>
+        </li>
+      </ul>
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        onClick={onClearCompleted}
+        disabled={!hasCompleted}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
