240dfeb6cd71a7d07a844b0b000e0515a22e6373
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-10-13 18:29:57 +0300

    fix all tests

--- src/types/hooks/errorMessage.tsx
+++ src/types/hooks/errorMessage.tsx
@@ -1,7 +1,7 @@
 export enum ErrorMessage {
   UnableToLoad = 'Unable to load todos',
-  UnableToAdd = 'Unable to add todo',
-  UnableToDelete = 'Unable to delete todo',
+  UnableToAdd = 'Unable to add a todo',
+  UnableToDelete = 'Unable to delete a todo',
   UnableToUpdate = 'Unable to update todo',
   TitleShouldNotBeEmpty = 'Title should not be empty',
 }
